7be02c89cc67a5ccaaeca6fd015c4214c1a67882
lanemunds <lanemunds@gmail.com>
2023-03-24 15:23:13 -0600

    fixed requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 from models import db
+import sqlite3
 
 # Flask
 app = Flask(__name__)
@@ -12,16 +13,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 db.init_app(app)
 
-# db.create_all()
+db.create_all()
 
 
 @app.route("/")
 def index():
-    '''
-    Home page
-    '''
-    return "Hello World"
+ 
+    return render_template('home.html')
 
+
+@app.route('/user/<user_id>')
+def user_page(user_id):
+    return render_template('user.html')
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+
+    new_task = ToDo(name=name)
 
 
 if __name__ == "__main__":
